b467c8d84a4c1daf3bb7debebd4118511b200ab6
Jason Tarka <jason@tarka.ca>
2017-05-05 16:08:28 -0400

    Modify `editInPlace` to include select lists
    - It made sense to include these as part of click-to-edit, rather than essentially rewriting CTE
    - Using "value" rather than "contents" as "value" has better semantic meaning
    - Annoying issue of the input title moving that i wasn't able to resolve without doing some hackery

--- controls/clickToEdit/control.js
+++ controls/clickToEdit/control.js
@@ -7,13 +7,17 @@ const KeyCodes = {
 };
 
 class ClickToEdit {
+
 	constructor( node, data ) {
 		this.data = JSON.parse( data );
 		this.title = this.data.title;
+		this.isSelect = this.data.type === 'select';
+		this.selectMap = this.isSelect ? new Map() : null;
 
-		this.contents = this.data.contents;
+		this.value = this.data.value;
 
 		this.elements = {
+			root: $( '.clickToEdit', node ),
 			button: $( 'button', node ),
 			contents: $( '.display .contents', node ),
 			display: $( '.display', node ),
@@ -26,7 +30,7 @@ class ClickToEdit {
 	}
 
 	init() {
-		var title = this.data.title;
+		let title = this.data.title;
 		this.elements.title.text( title );
 
 		this.elements.button
@@ -35,26 +39,95 @@ class ClickToEdit {
 			.on( 'focus', () => this.editButtonFocus() )
 			.on( 'blur', () => this.editButtonBlur() );
 
-		this.updateEditButton();
-
 		this.elements.contents
-			.text( this.data.contents )
 			.on( 'click', () => this.contentsClick() );
 
+		if( this.isSelect ) {
+			this.elements.input.remove();
+			this.elements.input = this._constructSelect();
+			this.elements.inputContainer.append( this.elements.input );
+			this.elements.root.addClass( 'select' );
+		} else {
+			this.elements.input
+			    .attr( 'name', this.data.name );
+		}
+
+		this.updateEditButton();
+		this._updateDisplayedValue();
+
+		// Information on what just happened, and how to finish editing.
+		// Will be read out when focusing in the field, before the contents.
 		this.elements.input
-			.attr( 'name', this.data.name )
-			// Information on what just happened, and how to finish editing.
-			// Will be read out when focusing in the field, before the contents.
-			.attr( 'aria-label', `Editing ${title}. Hit Tab to finish, or Escape to cancel.` )
+		    .attr( 'aria-label', `Editing ${title}. Hit Tab to finish, or Escape to cancel.` )
 			.on( 'keydown', e => this.inputKeyDown( e ) )
 			.on( 'keyup', e => this.inputKeyUp( e ) )
-			.on( 'blur', () => this.inputBlur() );
+			.on( 'blur', () => this.inputBlur() )
+	}
+
+	_constructSelect() {
+		const name = this.data.name,
+			options = this.data.options;
+
+		options.forEach( data => this._fillSelectMap( data ) );
+
+		return $( `
+				<select name="${name}">
+					<option value=""
+							aria-label="No value"
+					>
+						---
+					</option>
+				</select>`
+			).append( options.map( data => this._constructOption( data ) ) );
+	}
+
+	_fillSelectMap( option ) {
+		if( option.options ) {
+			return option.options.forEach( data => this._fillSelectMap( data ) );
+		}
+
+		this.selectMap.set( option.value, option.text );
+	}
+
+	/**
+	 * @param {OptionData} data
+	 * @private
+	 */
+	_constructOption( data ) {
+		const text = data.text,
+			value = data.value,
+			selected = data.selected ? 'selected="selected"' : '',
+			subOptions = data.options;
+
+		if( subOptions != null ) {
+			return this._constructOptionGroup( text, subOptions );
+		}
+
+		return $( `
+				<option value="${value}"
+						${selected}
+				>
+					${text}
+				</option>`
+		);
+	}
+
+	/**
+	 * @param {string} text
+	 * @param {Array<OptionData>} subOptions
+	 * @private
+	 */
+	_constructOptionGroup( text, subOptions ) {
+		let options = subOptions.map( data => this._constructOption( data ) );
+		return $( `
+				<optgroup label="${text}">
+			` ).append( options );
 	}
 
 	/***** Specifically around editing *****/
 
 	activateEdit() {
-		this.elements.input.val( this.contents );
+		this.elements.input.val( this.value );
 
 		this.elements.display.addClass( 'hidden' );
 		this.elements.inputContainer.removeClass( 'hidden' );
@@ -66,12 +139,12 @@ class ClickToEdit {
 	}
 
 	saveEdit() {
-		this.contents = this.elements.input.val();
+		this.value = this.elements.input.val();
 		this.endEdit();
 	}
 
 	endEdit() {
-		this.elements.contents.text( this.contents );
+		this._updateDisplayedValue();
 		this.elements.display.removeClass( 'hidden' );
 		this.elements.inputContainer.addClass( 'hidden' );
 
@@ -82,7 +155,24 @@ class ClickToEdit {
 	}
 
 	updateEditButton() {
-		this.elements.button.text( `${this.title}: ${this.contents}. Activate to edit.` );
+		const text = this.value
+			? this.isSelect
+				? this.selectMap.get( this.value )
+				: this.value
+			: 'No Value';
+		this.elements.button.text( `${this.title}: ${text}. Activate to edit.` );
+	}
+
+	_updateDisplayedValue() {
+		const value = this.value;
+
+		if( value ) {
+			this.elements.contents.removeClass( 'empty' )
+				.text( this.isSelect ? this.selectMap.get( value ) : value );
+		} else {
+			this.elements.contents.addClass( 'empty' )
+				.text( this.title );
+		}
 	}
 
 	/***** Event handling *****/
